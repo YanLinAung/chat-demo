9d7730a02f1b2f6e80bcb3ef622e6a513859eb23
Yan Lin Aung <yanlinaung2012@gmail.com>
2023-10-16 17:32:32 +0630

    format and add user name

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,82 @@
 <script setup>
 
 import {nextTick, ref} from "vue";
-  import {addMessage, getMessages} from "@/messages";
+import {addMessage, getMessages} from "@/messages";
 
-  const user = ref(null)
-  const messages = ref([])
-  const scrollRef = ref(null)
-  const inputMessage = ref('')
+const user = ref(null)
+const messages = ref([])
+const scrollRef = ref(null)
+const inputMessage = ref('')
 
-  const loadMessages = (id = null) => {
-    if(id === null) {
-      return getMessages()
-          .then((rows) => {
-            messages.value = rows.reverse()
-            return rows.length
-          })
-    } else {
-      return getMessages(id)
-          .then((rows) => {
-            messages.value = rows.reverse().concat(messages.value)
-            return rows.length
-          })
-    }
-  }
-
-  const handleScroll = (e) => {
-    let element = e.target
-
-    if(element.scrollTop < 10) {
-      let first = messages.value[0]
-      console.log('loading...' + first.id)
-      loadMessages(first.id)
-          .then((size) => {
-            if(size !== 0) {
-              element.scrollTo({top: 50})
-            }
-          })
-    }
-  }
-
-  const sendMessage = () => {
-    if(inputMessage.value !== '') {
-      addMessage(user.value, inputMessage.value)
-          .then(() => inputMessage.value = '')
-    }
-  }
-
-  new BroadcastChannel('chat-data').onmessage = (msg) => {
-    messages.value.push(msg.data)
-    nextTick(() => {
-      scrollRef.value &&
-      scrollRef.value.scrollTo(0, scrollRef.value.scrollHeight)
-    })
-  }
-
-  const login = (e) => {
-    user.value = e.target.value
-    loadMessages()
-        .then(() => {
-          scrollRef.value.scrollTo(0, scrollRef.value.scrollHeight)
+const loadMessages = (id = null) => {
+  if (id === null) {
+    return getMessages()
+        .then((rows) => {
+          messages.value = rows.reverse()
+          return rows.length
+        })
+  } else {
+    return getMessages(id)
+        .then((rows) => {
+          messages.value = rows.reverse().concat(messages.value)
+          return rows.length
         })
   }
+}
+
+const handleScroll = (e) => {
+  let element = e.target
+
+  if (element.scrollTop < 10) {
+    let first = messages.value[0]
+    console.log('loading...' + first.id)
+    loadMessages(first.id)
+        .then((size) => {
+          if (size !== 0) {
+            element.scrollTo({top: 50})
+          }
+        })
+  }
+}
+
+const sendMessage = () => {
+  if (inputMessage.value !== '') {
+    addMessage(user.value, inputMessage.value)
+        .then(() => inputMessage.value = '')
+  }
+}
+
+new BroadcastChannel('chat-data').onmessage = (msg) => {
+  messages.value.push(msg.data)
+  nextTick(() => {
+    scrollRef.value &&
+    scrollRef.value.scrollTo(0, scrollRef.value.scrollHeight)
+  })
+}
+
+const login = (e) => {
+  user.value = e.target.value
+  loadMessages()
+      .then(() => {
+        scrollRef.value.scrollTo(0, scrollRef.value.scrollHeight)
+      })
+}
 </script>
 
 <template>
   <div v-if="user === null"
-    class="content-center">
+       class="content-center">
     <div
         class="flex flex-col justify-between min-w-max mx-auto my-3
         block p-6 bg-white border border-gray-200 rounded-lg shadow w-[500px]">
-        <div class="mb-6">
-          <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Enter Username and Press Enter
-          </label>
-          <input type="text" id="username" placeholder="Enter user name" @keydown.enter="login"
-                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+      <div class="mb-6">
+        <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Enter Username and Press Enter
+        </label>
+        <input type="text" id="username" placeholder="Enter user name" @keydown.enter="login"
+               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
                 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-        </div>
+      </div>
     </div>
   </div>
 
@@ -84,13 +84,15 @@ import {nextTick, ref} from "vue";
     <div class="flex flex-col  h-auto justify-between min-w-max mx-auto my-3
   block p-6 bg-white border border-gray-200 rounded-lg shadow w-[500px]">
 
+      <h4 class="text-3xl text-gray-900 mb-4 text-center">{{ user }}</h4>
+      
       <div @scroll="handleScroll" ref="scrollRef"
-          class="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue w-full
+           class="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue w-full
           scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
 
         <div v-for="message in messages">
             <span class="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">
-              <span class="font-bold font-italic">{{message.id }} - {{ message.name }}</span>
+              <span class="font-bold font-italic">{{ message.id }} - {{ message.name }}</span>
                 {{ message.content }}
             </span>
         </div>
@@ -99,7 +101,7 @@ import {nextTick, ref} from "vue";
 
       <div class="border-t-2 border-gray-200 pt-4 mt-2">
         <form @submit.prevent="sendMessage"
-            class="flex gap-x-1">
+              class="flex gap-x-1">
           <input type="text" placeholder="Write your message"
                  v-model="inputMessage"
                  @keydown.enter="sendMessage"
