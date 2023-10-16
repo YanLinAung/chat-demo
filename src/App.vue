1b759d4d8d9a2d2f97d9995dc519e85d745914cf
Yan Lin Aung <yanlinaung2012@gmail.com>
2023-10-16 17:41:44 +0630

    remove msg id and add color

--- src/App.vue
+++ src/App.vue
@@ -92,7 +92,7 @@ const login = (e) => {
 
         <div v-for="message in messages">
             <span class="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">
-              <span class="font-bold font-italic">{{ message.id }} - {{ message.name }}</span>
+              <span class="font-bold font-italic text-blue-900">{{ message.name }}</span>
                 {{ message.content }}
             </span>
         </div>
